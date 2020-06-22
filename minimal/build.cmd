144be6f90a0bd2e28f11d55d99f6cb91940be86b
joric <joric@users.noreply.github.com>
2020-06-22 13:07:36 +0500

    updated files

--- minimal/build.cmd
+++ minimal/build.cmd
@@ -2,24 +2,21 @@
 
 cd %~dp0
 
-rd /S /Q .\_build
-
-set file=_build/nrf52840_xxaa.hex
+rd /S /Q _build
 
 bash -c "export NRFSDK15_ROOT=/mnt/c/SDK/nRF5_SDK_15.0.0_a53641a && make" || exit
 
 ::uf2conv.py %file% -c -b 0xFDE00 -f 0xADA52840 -o bmpapi.uf2 || exit
 
-uf2conv.py %file% -c -f 0xADA52840 -o bmpapi.uf2 || exit
+uf2conv.py _build/nrf52840_xxaa.hex -c -f 0xADA52840 -o bmpapi.uf2 || exit
 
+echo Resetting via microshell...
 echo dfu| plink.exe -serial com9
 
 echo Waiting for the UF2 drive. Press Reset twice...
 
 :loop
 if not exist e:\ goto :loop
-
-timeout 1
 
 copy bmpapi.uf2 e:\
 
--- minimal/main.c
+++ minimal/main.c
@@ -1,22 +1,23 @@
 #include "apidef.h"
 #include "bmpapi.h"
 
-#define LOCATE_FUNC  __attribute__((__section__(".mysection")))
-
-void LOCATE_FUNC bootloader_jump(void) {
+void bootloader_jump(void) {
   nrf_gpio_cfg_output(LED_PIN);
   for (int i=0; i<4; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
 }
 
-/*
-bmp_api_t bmpapi_static = {
-  .api_version = 0,
-  .bootloader_jump = bootloader_jump
+void reset(uint32_t _) { }
+
+__attribute__((section(".api_table")))
+bmp_api_t API_TABLE = {
+    .api_version = 0x01020304,
+    .bootloader_jump = bootloader_jump,
+    //.app = { reset, enter_sleep_mode, /* ... */ },
+    // ...
 };
-*/
 
 int main(void) {
   bootloader_jump();
-  ((void(*)(void))0xFDE00)();
+  BMPAPI->bootloader_jump(); // crash
 }
 
