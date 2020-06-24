3761bb99769fd8dadbdcc7b9d1143e7983ab0eea
joric <joric@users.noreply.github.com>
2020-06-24 14:29:21 +0500

    updated files

--- launcher/main.c
+++ launcher/main.c
@@ -16,17 +16,18 @@ int main(void) {
     NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
   }
 
-  // 1 flash to indicate launcher started
+  // 5 short flash to indicate launcher started
   nrf_gpio_cfg_output(LED_PIN);
-  for (int i=0; i<2; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
+  for (int i=0; i<10; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(50); }
 
-  BMPAPI->bootloader_jump(); // crash!
+  BMPAPI->bootloader_jump(); // works!
+  BMPAPI->logger.init(); // works!
 
   nrf_delay_ms(500);
 
-  // 1 flash to indicate launcher finished
+  // 5 short flash to indicate launcher finished
   nrf_gpio_cfg_output(LED_PIN);
-  for (int i=0; i<2; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
+  for (int i=0; i<10; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(50); }
 
 }
 
--- minimal/main.c
+++ minimal/main.c
@@ -5,16 +5,19 @@
 #define LED_PIN GPIO(1,10)
 
 void bootloader_jump(void) {
+  // 2 long flashes
   nrf_gpio_cfg_output(LED_PIN);
   for (int i=0; i<4; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
 }
 
 void logger_init(void) {
+  // 4 short flashes
   nrf_gpio_cfg_output(LED_PIN);
-  for (int i=0; i<8; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
+  for (int i=0; i<8; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(100); }
 }
 
 int main(void) {
   BMPAPI->bootloader_jump();
+  BMPAPI->logger.init();
 }
 
