d8f8dbeaa6d8cdd21a775232c0793d44bcf816ac
joric <joric@users.noreply.github.com>
2020-06-26 04:48:37 +0500

    updated files

--- minimal/main.c
+++ minimal/main.c
@@ -4,16 +4,17 @@
 #define GPIO(port, pin) ((port << 6) | pin)
 #define LED_PIN GPIO(1,10)
 
-void bootloader_jump(void) {
-  // 2 long flashes
+void blink(int times, int delay) {
   nrf_gpio_cfg_output(LED_PIN);
-  for (int i=0; i<4; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(250); }
+  for (int i=0; i<times*2; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(delay); }
+}
+
+void bootloader_jump(void) {
+  blink(2,200);
 }
 
 void logger_init(void) {
-  // 4 short flashes
-  nrf_gpio_cfg_output(LED_PIN);
-  for (int i=0; i<8; i++) { nrf_gpio_pin_write(LED_PIN, i%2==0); nrf_delay_ms(100); }
+  blink(2,100);
 }
 
 int main(void) {
